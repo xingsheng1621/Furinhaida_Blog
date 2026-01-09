bd48972c16507f89d9e16b5a03a7605333808964
xingsheng1621 <1621342655@qq.com>
2026-01-09 17:58:03 +0800

    2026.01.09-公司

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -16,59 +16,34 @@ const getData = async ({ slug }) => {
 
 const SinglePage = async ({ params }) => {
 
-
   const {slug} = params;
-  const data = await getData(params);
+
+  const data = await getData({ slug });
 
   return (
     <div className={styles.container}>
         <div className={styles.infoContainer}>
           <div className={styles.textContainer}>
-            <h1 className={styles.title}>这是一篇博客文章的标题</h1>
+            <h1 className={styles.title}>
+              {data?.title}
+              </h1>
             <div className={styles.user}>
-              <div className={styles.userImageContainer}>
+              {data?.img && (<div className={styles.userImageContainer}>
                 <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
-              </div>
+              </div>)}
               <div className={styles.userTextcontainer}>
                 <span className={styles.username}>Username</span>
                 <span className={styles.date}>2025.12.17</span>
               </div>
             </div>
           </div>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
+          {data?.img && (<div className={styles.imageContainer}>
+            <Image src={data?.img} alt="" fill className={styles.image} />
+          </div>)}
         </div>
         <div className={styles.content}>
           <div className={styles.post}>
-            <div className={styles.description}>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur 
-              adipiscing elit. Sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua.
-              </p>
-              <h2>Subheading 1</h2>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur 
-              adipiscing elit. Sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua.
-              </p>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur 
-              adipiscing elit. Sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua.
-              </p>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur 
-              adipiscing elit. Sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua.
-              </p>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur 
-              adipiscing elit. Sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
+            <div className={styles.description} dangerouslySetInnerHTML={{__html:data.description}} />
             <div className={styles.comments}>
               <Comments/>
             </div>
